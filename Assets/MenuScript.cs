46dddc99dc8b8cc1ee6f52ebd513294d49ef9299
Nicolas Langley <nicolasqlangley@gmail.com>
2014-01-15 23:37:11 -0500

    Added initial Unity Project commit

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	void OnGUI () {
+		GUI.Box (new Rect ((Screen.width / 2) - 100, (Screen.height / 2) - 80, 200, 25), "BATTLESHIP");
+		GUI.Box (new Rect ((Screen.width / 2) - 50, (Screen.height / 2) - 30, 100, 25), "Single Player"); 
+		GUI.Box (new Rect ((Screen.width / 2) - 50, (Screen.height / 2) + 30, 100, 25), "Multi Player");
+	}
+}
