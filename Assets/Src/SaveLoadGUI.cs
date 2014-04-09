1b2fe4f6fd22b5ad6ffea77b91d7e18e23a8b380
Peter Davoust <worldgnat@gmail.com>
2014-04-09 02:26:01 -0400

    I did stuff.

--- Assets/Src/SaveLoadGUI.cs
+++ Assets/Src/SaveLoadGUI.cs
@@ -9,21 +9,16 @@ public class SaveLoadGUI : MonoBehaviour {
 	GameSaverScript gameSaver;
 	
 	void OnGUI() {
-		if(GUI.Button(new Rect(Screen.width-150,10,100,80), "Load")) {
+		/*if(GUI.Button(new Rect(Screen.width-150,10,100,80), "Load")) {
 			GameSaverScript.Load(Path.Combine(Application.persistentDataPath, "ships_saved.xml"),GetComponent<GameScript>());
 			Debug.Log("Loaded " + Path.Combine(Application.persistentDataPath, "ships_saved.xml").ToString());
-			/*
-			 * Things we need to set for things on load:
-			 * -ShipScript.gameScript
-			 * -ShipScript.gridScript
-			 * -ShipScript.rpcScript
-			 */
 
-		}
+		}*/
 		if (GUI.Button (new Rect (Screen.width-150, 90, 100, 80), "Save")) {
 			gameSaver = new GameSaverScript(GetComponent<GameScript>());
-			gameSaver.Save(Path.Combine(Application.persistentDataPath, "ships_saved.xml"));
-			Debug.Log("Saved to " + Path.Combine(Application.persistentDataPath, "ships_saved.xml").ToString());
+			string filename = gameSaver.myname + "_" + gameSaver.opponentname + ".xml";
+			gameSaver.Save(Path.Combine(Application.persistentDataPath, filename));
+			Debug.Log("Saved to " + Path.Combine(Application.persistentDataPath, filename).ToString());
 		}
 
 	}
